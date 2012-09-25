008ec99f66eec3e61032eb41f458690bbeadf3ef
moacirmoda <moa.moda@gmail.com>
2012-09-25 17:56:01 -0300

    adding a boolean field in userprofile called is_ccn

--- lilacs_submission/account/models.py
+++ lilacs_submission/account/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from django.db import models
 from center.models import *
@@ -6,4 +7,5 @@ class UserProfile(models.Model):
 
     user = models.ForeignKey(User, unique=True)
     center = models.ForeignKey(Center, null=True)
-    is_admin = models.BooleanField("is center administrator?")
+    is_admin = models.BooleanField(_("is center administrator?"))
+    is_ccn = models.BooleanField(_("is national center?"))
