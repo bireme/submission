fdd284d36351ee65ef8d9b3087a696a20b470bdc
moacirmoda <moa.moda@gmail.com>
2013-01-17 18:26:41 -0200

    #73: updating admin settings

--- bireme/submission/admin.py
+++ bireme/submission/admin.py
@@ -24,16 +24,20 @@ class BibliographicTypeLocalAdmin(admin.StackedInline):
     exclude = ('created', 'creator', 'updated', 'updater')
 
 class StepAdmin(GenericAdmin):
+    inlines = [StepLocalAdmin, ]
+    
     list_display = ('title', 'parent', 'creator', 'created', 'updater', 'updated')
     list_filter = ('type', 'finish', 'pending')
-    inlines = [StepLocalAdmin, ]
+    search_fields = ('title', )
 
 class BibliographicTypeAdmin(GenericAdmin):
     list_display = ('title', 'creator', 'created', 'updater', 'updated')
+    search_fields = ('title', )
     inlines = [BibliographicTypeLocalAdmin, ]
 
 class TypeAdmin(GenericAdmin):
     list_display = ('title', 'creator', 'created', 'updater', 'updated')
+    search_fields = ('title', )
 
 class LildbiVersionAdmin(GenericAdmin):
     list_display = ('title', 'creator', 'created', 'updater', 'updated')
@@ -41,6 +45,8 @@ class LildbiVersionAdmin(GenericAdmin):
 
 class SubmissionAdmin(GenericAdmin):
     list_display = ('id', 'type', 'current_status', 'creator')
+    search_fields = ('title', )
+    list_filter = ('current_status', 'type')
 
 admin.site.register(Submission, SubmissionAdmin)
 admin.site.register(Step, StepAdmin)
