ce2e0207f32b3cd19138188b8019de83e8174927
moacirmoda <moa.moda@gmail.com>
2013-03-05 11:30:02 -0300

    changing normalize permissions

--- tools/normalize-permissions.sh
+++ tools/normalize-permissions.sh
@@ -1,6 +1,15 @@
-chown -R root:bvs ../bireme/
-chown -R apache:bvs  ../bireme/media
-chown -R apache:bvs  ../bireme/whoosh
+#!/bin/bash
 
-find ../bireme -type f | xargs chmod 664
-find ../bireme -type d | xargs chmod 775 
+if [ $# -eq 1 ]; then
+    PROJECT_PATH=$1
+else
+    echo "ERROR: Please fill the first parameter with path."
+    exit 1
+fi
+
+chown -R root:bvs $PROJECT_PATH/bire$PROJECT_PATH/
+chown -R apache:bvs  $PROJECT_PATH/bire$PROJECT_PATH/media
+chown -R apache:bvs  $PROJECT_PATH/bire$PROJECT_PATH/whoosh
+
+find $PROJECT_PATH/bireme -type f | xargs chmod 664
+find $PROJECT_PATH/bireme -type d | xargs chmod 775 
