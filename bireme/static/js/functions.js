134cb074598d41cb03a19561751443462a4d93b6
moacirmoda <moa.moda@gmail.com>
2013-03-25 17:03:56 -0300

    move language function to functions.js

--- bireme/static/js/functions.js
+++ bireme/static/js/functions.js
@@ -2,3 +2,16 @@
 function open_message(id_message) {
     $(id_message).toggle('fast');
 }
+
+// change language
+function change_language(lang) {
+    
+    // chama, via ajax, a view que armazena a linguagem em cookie, para relembrar na proxima entrada
+    $.get("{% url main.views.cookie_lang %}", { language: lang} , function(data) {
+        
+        var form = document.language;
+        form.language.value = lang;
+        form.submit();
+    });
+
+    }
